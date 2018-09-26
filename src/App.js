9d3c3621250ee2aca9ee14b914ad290ceec396e7
Admin User <adminuser@elements-mac114.local>
2018-09-26 14:28:06 -0600

    added sample html and js to embed in this app to App.js

--- src/App.js
+++ src/App.js
@@ -21,3 +21,39 @@ class App extends Component {
 }
 
 export default App;
+
+//Calebs example HTML to Embed secure method #2.  Employ this in example app.
+// <html>
+//
+// <head>
+//     <title>Simulate Send Token</title>
+//     <meta http-equiv="Cache-Control" content="no-store" />
+//
+//
+//     <script src="javascripts/jquery.min.js" type="text/javascript"></script>
+//     <!-- <script src="javascripts/send-token.js" type="text/javascript"></script> -->
+//     <script src="https://heimdall.stgc0.uswest2.c-e.works/v1/public/javascripts/heimdall-sdk-staging.js" type="text/javascript"></script>
+// </head>
+//
+// <body>
+//     <script>
+//         const callback = function (instance) {
+//             document.getElementById('payload').innerHTML = ""
+//             console.log(instance)
+//             document.getElementById('payload').innerHTML = JSON.stringify(instance, null, 2)
+//         }
+//         let urlParams = new URLSearchParams(window.location.search);
+//         let token = urlParams.get('token');
+//         const create = () => {
+//             CE.createInstance({ token }, callback)
+//         }
+//     </script> Simulate Launch Connector
+//     <br>
+//     <br>
+//     <button id="connect" onclick="create()">Connect to Anything</button>
+//     <div id="payload">
+//
+//     </div>
+// </body>
+//
+// </html>
